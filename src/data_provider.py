997c9ef769e0c80679f9c2b1e14b2a9d0de7bc14
Konrad Kapron <Kapron.K4@gmail.com>
2018-12-13 22:03:22 +0100

    Added ngrams

--- src/data_provider.py
+++ src/data_provider.py
@@ -11,27 +11,55 @@ class NoStemmer:
 def get_data(source, representation='bag_of_words', stemmer=NoStemmer()):
     with open(source) as s:
         data = json.load(s)
-    print(len(data['jokes']))
-    return globals()['get_data_as_' + representation](data, stemmer)
+    data = globals()['get_data_as_' + representation](data, stemmer)
+    #reset_bag_of_words() #uncomment if using get_data more than once
+    return data
 
 
 def get_data_as_bag_of_words(data, stemmer):
-    return [(to_bag_of_words(joke['joke'], stemmer), 
+    return bag_of_words(data, stemmer, get_word, iter_word)
+
+
+def get_data_as_ngrams(data, stemmer):
+    return bag_of_words(data, stemmer, get_ngram, iter_ngram)
+
+
+def bag_of_words(data, stemmer, key_getter, key_iter):
+    return [(to_bag_of_words(joke['joke'], stemmer, key_getter, key_iter),
               to_categorical(joke['categories']))
              for joke in data['jokes']]
     
 
-def to_bag_of_words(joke, stemmer):
+def to_bag_of_words(joke, stemmer, key_getter, key_iter):
     try:
-        return [to_bag_of_words.word_indices[stemmer.stem(word.lower())] 
-                for word in re.findall(r'\w+', joke)]
+        return [to_bag_of_words.word_indices[key_getter(key, stemmer)]`
+                for key in key_iter(joke)]
     except KeyError as error:
-        to_bag_of_words.word_indices[error.args[0].lower()] = to_bag_of_words.index
+        to_bag_of_words.word_indices[error.args[0]] = to_bag_of_words.index
         to_bag_of_words.index += 1
-        return to_bag_of_words(joke, stemmer)
+        return to_bag_of_words(joke, stemmer, key_getter, key_iter)
 
-to_bag_of_words.word_indices = {}
-to_bag_of_words.index = 0
+def reset_bag_of_words():
+    to_bag_of_words.word_indices = {}
+    to_bag_of_words.index = 0
+
+reset_bag_of_words()
+
+
+def get_ngram(ngram, stemmer):
+    return " ".join(get_word(word, stemmer) for word in ngram)
+
+
+def get_word(word, stemmer):
+    return stemmer.stem(word.lower())
+
+
+def iter_ngram(jokes, grams=2):
+    return nltk.ngrams(iter_word(jokes), grams)
+
+
+def iter_word(joke):
+    return re.findall(r'\w+', joke)
 
 
 def to_categorical(categories):
@@ -45,7 +73,9 @@ to_categorical.t = {}
 
 if __name__ == '__main__':
     import sys
-    print(get_data(sys.argv[1], stemmer=nltk.stem.snowball.SnowballStemmer('english'))[:10])
-    print("Number of unique words after stemming: " + str(to_bag_of_words.index))
+    print(get_data(sys.argv[1],
+                   representation='bag_of_words',
+                   stemmer=nltk.stem.lancaster.LancasterStemmer())[:10])
+    print("Number of unique items after stemming: " + str(to_bag_of_words.index))
     print("Number of categories: " + str(len(to_categorical.t)))
     print("Categories: " + str(to_categorical.t.keys()))
